117b4e4751131061984a4b73f729556039d3d532
Ian Reah <ian.reah@nonlinear.com>
2013-12-11 13:16:54 +0000

    Add grunt-contrib-watch and configure it to run jshint whenever a javascript file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,13 @@ module.exports = function(grunt) {
       options: {
         ignores: ['scripts/jquery.tmpl.js', 'scripts/jquery-1.7.1.min.js', 'scripts/knockout-2.0.0.js']
       }
+    },
+    watch: {
+      files: ['**/*.js'],
+      tasks: ['jshint'] // TODO: Don't forget to run the tests when files change as well when I add them!
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 };
