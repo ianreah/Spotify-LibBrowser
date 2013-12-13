eea1c5b57a8b57c8dad63068a9ab6aeccd915963
Ian Reah <ian.reah@gmail.com>
2013-12-13 20:55:21 +0000

    Register a default grunt task
    So running 'grunt' on the command line will run jshint immediately and then the watch task to watch for changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,4 +14,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'watch']);
 };
