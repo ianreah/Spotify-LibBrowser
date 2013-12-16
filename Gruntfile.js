411cd72231e8d4ceffb1f79e4e24d960f7ca5039
Ian Reah <ian.reah@nonlinear.com>
2013-12-16 12:30:55 +0000

    Added a simple jasmine unit test
    The album model is a simple enough class to start testing. I suspect adding more tests is going to require some refactoring &/or mocking.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,28 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'scripts/**/*.js'], // TODO: Don't forget to lint the tests as well when I add them!
+      files: ['Gruntfile.js', 'scripts/**/*.js', 'specs/**/*.js'],
       options: {
         ignores: ['scripts/jquery.tmpl.js', 'scripts/jquery-1.7.1.min.js', 'scripts/knockout-2.0.0.js']
       }
     },
+    jasmine: {
+      src: ['scripts/album.js'], // Just one simple file for now!
+      options: {
+        helpers: ['specs/spechelper.js'],
+        specs: ['specs/**/*.spec.js'],
+        vendor: ['scripts/knockout-2.0.0.js']
+      }
+    },
     watch: {
       files: ['**/*.js'],
-      tasks: ['jshint'] // TODO: Don't forget to run the tests when files change as well when I add them!
+      tasks: ['jshint', 'jasmine']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.registerTask('default', ['jshint', 'jasmine', 'watch']);
 };
---
+++ specs/album.spec.js
@@ -0,0 +1,13 @@
+﻿describe("Album model", function () {
+    it("should set name when constructed from a spotify album", function () {
+        var mockSpotifyAlbum = {			
+            artist: "Led Zeppelin",
+            cover: "spotify:image:c89be3d95870abb652c16deef6e3d3e5174710ff",
+            name: "Led Zeppelin IV",
+            uri: "spotify:album:1Ugdi2OTxKopVVqsprp5pb"
+        };
+        
+        var album = new Album(mockSpotifyAlbum);
+        expect(album.name()).toBe("Led Zeppelin IV");
+    });
+});
